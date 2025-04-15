c6f3593bc128a4c0644c745d0b9b50e26af70028
sarita202422 <smarinao2024@alu.uct.cl>
2025-04-14 20:12:28 -0400

    commit 2
    agregar productos

--- script.js
+++ script.js
@@ -14,19 +14,19 @@ document.getElementById("boton").addEventListener("click", () => {
 
 // Caso 2: Cambio de productos
 document.getElementById("btn1").addEventListener("click", () => {
-    document.getElementById("img").src = "producto1.jpg";
-    document.getElementById("nombreProducto").innerText = "Smartphone x1";
-    document.getElementById("desc").innerText = "Descripción del producto 1";
+    document.getElementById("img").src = "img/smartphone x1.png";
+    document.getElementById("nombreProducto").innerText = "Smartphone X1";
+    document.getElementById("desc").innerText = "El nuevo Smartphone X1 con cámara de 108MP y pantalla AMOLED.";
 });
 
 document.getElementById("btn2").addEventListener("click", () => {
-    document.getElementById("img").src = "producto2.jpg";
+    document.getElementById("img").src = "img/auriculares.png";
     document.getElementById("nombreProducto").innerText = "Auriculares bluetooth";
-    document.getElementById("desc").innerText = "Descripción del producto 2";
+    document.getElementById("desc").innerText = "Sonido de alta calidad, perfecto para hacer deporte o escuchar música sin cables.";
 });
 
 document.getElementById("btn3").addEventListener("click", () => {
-    document.getElementById("img").src = "producto3.jpg";
-    document.getElementById("nombreProducto").innerText = "Producto 3";
-    document.getElementById("desc").innerText = "Descripción del producto 3";
+    document.getElementById("img").src = "img/laptop.png";
+    document.getElementById("nombreProducto").innerText = "Laptop ultradelgada Z8";
+    document.getElementById("desc").innerText = "La laptop ultradelgada Z8 con procesador Intel i7, ideal para trabajo y entretenimiento.";
 });
